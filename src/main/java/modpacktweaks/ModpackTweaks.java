19d5f27ba417581f547251cd6d4da1fe2d76401a
tterrag1098 <tterrag1098@gmail.com>
2015-01-05 22:52:47 -0500

    Remove manual config stuff as it was moved to ttCore. Add ttCore dep.

--- src/main/java/modpacktweaks/ModpackTweaks.java
+++ src/main/java/modpacktweaks/ModpackTweaks.java
@@ -29,7 +29,7 @@ import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.event.FMLServerStartingEvent;
 
-@Mod(modid = "modpackTweaks", name = "ModpackTweaks", version = ModpackTweaks.VERSION, dependencies = "required-after:NotEnoughItems;after:ThermalExpansion")
+@Mod(modid = "modpackTweaks", name = "ModpackTweaks", version = ModpackTweaks.VERSION, dependencies = "required-after:ttCore;required-after:NotEnoughItems;after:ThermalExpansion")
 public class ModpackTweaks
 {
 	public static final String VERSION = "1.2.0";
